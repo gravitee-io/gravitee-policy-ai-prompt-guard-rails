a01d5ff2d30fe93cd4617c0e65b08b54b5eba1dd
Thomas Ballivet <thomas.ballivet@graviteesource.com>
2026-03-12 18:14:40 +0100

    fix: do not block request when model detects no content violation

--- src/main/java/io/gravitee/policy/ai/prompt/guard/rails/AiPromptGuardRailsPolicy.java
+++ src/main/java/io/gravitee/policy/ai/prompt/guard/rails/AiPromptGuardRailsPolicy.java
@@ -81,8 +81,9 @@ public class AiPromptGuardRailsPolicy implements HttpPolicy {
                 .flatMapCompletable(classifierResults -> {
                     var detectedContentTypes = detectClassifierResultContentTypes(classifierResults, sensitivityThreshold);
                     if (
-                        configuration.parseContentChecks().isEmpty() ||
-                        configuration.parseContentChecks().stream().anyMatch(detectedContentTypes::contains)
+                        !detectedContentTypes.isEmpty() &&
+                        (configuration.parseContentChecks().isEmpty() ||
+                            configuration.parseContentChecks().stream().anyMatch(detectedContentTypes::contains))
                     ) {
                         logMetrics(detectedContentTypes, ctx, configuration.requestPolicy().getAction());
                         if (RequestPolicy.BLOCK_REQUEST.equals(configuration.requestPolicy())) {
